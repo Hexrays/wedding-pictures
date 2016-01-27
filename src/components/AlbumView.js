9a713021d203d28bf1fd5af56a263322149deb56
Jeffrey Denton <jeffd@hexrays.at>
2016-01-26 23:24:17 -0500

    Set images to preload and loaded images at the correct aspect ratio.

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -2,11 +2,17 @@ import React from 'react';
 // import Link from 'react-router';
 import {albumData} from '../constants/constants';
 import ThumbnailContainer from './ThumbnailContainer';
+import {Link} from 'react-router';
+import {Preload} from 'react-preload';
 
-const AlbumView = React.createClass({
+class AlbumView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = albumData[this.props.params.albumId];
+  }
 
-  render() {
-    const {title, count, thumbs, folder} = albumData[this.props.params.albumId];
+  componentWillMount() {
+    const {count, thumbs, folder} = this.state;
     if(!thumbs.length) {
       for (let i = 1; i < count; i++) {
         let fileNum;
@@ -21,16 +27,41 @@ const AlbumView = React.createClass({
         thumbs.push(url);
       }
     }
+  }
+
+  _handleImageLoadError() {
+    console.log('err');
+  }
+
+  _handleImageLoadSuccess() {
+    console.log('success');
+  }
+
+  render() {
+    const {title, count, thumbs} = this.state;
+    const loadingIndicator = (<div>Loading...</div>)
+    // let images = [];
 
     return (
       <div  >
+        <Link to='/'>Back</Link>
         <h3>{title}</h3>
         <p>Count: {count}</p>
-        <ThumbnailContainer thumbs={thumbs} />
+        <Preload
+          loadingIndicator={loadingIndicator}
+          images={thumbs}
+          autoResolveDelay={3000}
+          onError={this._handleImageLoadError}
+          onSuccess={this._handleImageLoadSuccess}
+          resolveOnError={true}
+          mountChildren={true}
+          >
+          <ThumbnailContainer thumbs={thumbs} />
+        </Preload>
       </div>
     );
   }
-});
+}
 
 
 export default AlbumView;
--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,18 +1,42 @@
 import React from 'react';
 
-const Thumbnail = ({
-    thumb
-}) => (
-    <div style={{
-        height:'200px',
-        width: '200px',
-        float: 'left',
-        margin: '5px',
-        backgroundImage: `url(${thumb})`,
-        backgroundSize: 'cover',
-        border: '1px solid black'
-    }} >
-    </div>
-);
+// const Thumbnail = ({
+//     thumb
+// }) => (
+
+// );
+
+class Thumbnail extends React.Component {
+  componentWillMount() {
+
+    // console.log(thumb);
+  }
+
+  render() {
+    const {thumb} = this.props;
+    let img = new Image();
+    img.src = thumb;
+    const imgHeight = 200;
+    const ratio = img.width/img.height;
+    let imgWidth = imgHeight * ratio;
+
+    // console.log();
+
+    return (
+      <div style={{
+          // height:{height}'px',
+          // width: '200px',
+          float: 'left',
+          margin: '5px',
+          // backgroundImage: `url(${thumb})`,
+          // backgroundSize: 'cover',
+          border: '1px solid black'
+      }} >
+        <img style={{verticalAlign:'middle'}} src={thumb} height={imgHeight} width={imgWidth} />
+
+      </div>
+    );
+  }
+}
 
 export default Thumbnail;
