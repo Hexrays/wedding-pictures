8ff05654df7c5bc6dc9fa113a50b93d475a0da2b
Jeffrey Denton <jeffd@hexrays.at>
2016-01-30 19:42:15 -0500

    Added react-photo-gallery and its amazing

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,19 +1,21 @@
 import React from 'react';
 // import Link from 'react-router';
 import {albumData} from '../constants/constants';
-import ThumbnailContainer from './ThumbnailContainer';
+// import ThumbnailContainer from './ThumbnailContainer';
+import Gallery from 'react-photo-gallery';
 import {Link} from 'react-router';
 
 class AlbumView extends React.Component {
 
   render() {
-    const {title, count, thumbs} = albumData[this.props.params.albumId];
+    const {title, count, photos} = albumData[this.props.params.albumId];
+    console.log(photos);
     return (
       <div  >
         <Link to='/'>Back</Link>
         <h3>{title}</h3>
         <p>Count: {count}</p>
-        <ThumbnailContainer thumbs={thumbs} />
+        <Gallery photos={photos} />
       </div>
     );
   }
