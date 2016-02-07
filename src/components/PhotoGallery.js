131e2631b4a2db6e0a114cd678ee999015053cc4
Jeffrey Denton <jeffd@hexrays.at>
2016-02-06 20:15:22 -0500

    Added the footer to the photogallery page

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,23 +1,33 @@
 require('styles/gallery.scss');
 
 import React from 'react';
-import {albumData} from '../constants/constants';
 import Gallery from 'react-photo-gallery';
 import {Link} from 'react-router';
+import Header from './Header';
+import Footer from './Footer';
+import {albumData} from '../constants/constants';
+
 
 class PhotoGallery extends React.Component {
 
   render() {
-    const {title, count, photos} = albumData[this.props.params.albumId];
+    const {title, count, photos, id} = albumData[this.props.params.albumId];
+    let nextAlbum = id === (albumData.length - 1) ? 0 : id + 1;
+
     return (
       <div className="page" >
+        <Header>
+          <h3>{title}</h3>
+        </Header>
         <Link to='/'>Back</Link>
-        <h3>{title}</h3>
         <p>Count: {count}</p>
         <section className="gallery">
           <Gallery className="gallery" photos={photos} />
         </section>
+        <Footer>
+          <Link to={`/album/${nextAlbum}`} >next</Link>
 
+        </Footer>
       </div>
     );
   }
