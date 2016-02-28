77827109d1ad70f8251a2cef9be6d02509c9c0c3
Jeffrey Denton <jeffd@hexrays.at>
2016-02-28 17:03:27 -0500

    Added instagram link to landing page

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -7,6 +7,9 @@ const AlbumList = (props) => (
         {props.albumData.map(album =>
           <Album {...album}  key={album.id} />
         )}
+        <li className="album insta">
+            <a className="album-insta-link" href="https://www.instagram.com/explore/tags/jeffkatieco/" target="_blank"><span>see more <br/> on instagram<br/>#jeffkatieco</span></a>
+        </li>
     </ul>
   </div>
 );
